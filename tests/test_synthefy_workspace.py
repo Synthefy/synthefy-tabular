c4f77907a6d33ac320cefb3ac1de56989297226e
Po-han Li <pohan@synthefy.com>
2026-09-08 16:28:48 -0500

    chore(release): prepare synthefy-nori 0.20.2 (#152)

--- tests/test_synthefy_workspace.py
+++ tests/test_synthefy_workspace.py
@@ -99,7 +99,7 @@ def test_project_identities_versions_and_build_backends_are_disjoint():
     client = _toml(_CLIENT / "pyproject.toml")
 
     assert root["project"]["name"] == "synthefy-nori"
-    assert root["project"]["version"] == "0.20.1"
+    assert root["project"]["version"] == "0.20.2"
     assert root["build-system"]["build-backend"] == "setuptools.build_meta"
     assert client["project"]["name"] == "synthefy"
     assert client["project"]["version"] == "7.1.2"
